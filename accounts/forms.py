b35f9a1e1dc104d69078a2367d623fdb0c6905e2
hi287 <san-j19001@sist.ac.jp>
2022-03-07 20:49:31 +0900

    Create photoProject

---
+++ accounts/forms.py
@@ -0,0 +1,8 @@
+from attr import field
+from django.contrib.auth.forms import UserCreationForm
+from .models import CustomUser
+
+class CustomUserCreationForm(UserCreationForm):
+    class Meta:
+        model=CustomUser
+        fields=('username','email','password1','password2')
